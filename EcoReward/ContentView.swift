b67733c0e42483e5ed5fcce4c1342bf0fc3aec46
Yang Xu <chcxuyang@gmail.com>
2020-09-02 15:53:25 +1000

    Initial Commit

---
+++ EcoReward/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcoReward
+//
+//  Created by Yang Xu on 2/9/20.
+//  Copyright © 2020 Yang Xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
